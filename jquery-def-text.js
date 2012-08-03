f58bb9515c73a6370ea7303e9b62604aeebbd40b
Eric Allen <eric@ericrallen.com>
2012-08-03 00:30:52 -0400

    Added support for default classes to be added and removed, as well as implemented a check for an already present value so we don't overwrite values inserted via GET, POST, etc.

--- jquery-def-text.js
+++ jquery-def-text.js
@@ -1,20 +1,44 @@
 (function( $ ) {
 	$.fn.defaultText = function(options) {
 		return this.each(function() {
+			//set default options and include user-defined options
 			var settings = $.extend({
-				def_text: ""
+				def_text : "",
+				def_class : ""
 			}, options);
-			$(this).val(settings.def_text);
-			$(this).focus(function() {
-				if($(this).val() == settings.def_text) {
-					$(this).val("");
-				}
-			});
-			$(this).blur(function() {
-				if($(this).val() == "") {
+			//check to see if the field already has a value
+			//this will prevent us from overwriting data inserted via GET or POST
+			if($(this).val() === '' || $(this).val() === settings.def_text) {
+				//if the field is empty
+				if($(this).val() === '') {
+					//we want to insert the default value
 					$(this).val(settings.def_text);
 				}
-			});
+				//set the default value
+				$(this).addClass(settings.def_class);
+				//when the field gets focus
+				$(this).focus(function() {
+					//if the value is the default text
+					if($(this).val() == settings.def_text) {
+						//remove the text and the default class
+						$(this).val("");
+						$(this).removeClass(settings.def_class);
+					}
+				});
+				//when the field loses focus
+				$(this).blur(function() {
+					//if the value is empty
+					if($(this).val() === "") {
+						//insert the default text and add the default class
+						$(this).val(settings.def_text);
+						$(this).addClass(settings.def_class);
+					}
+				});
+			//if there is already a value
+			} else {
+				//don't do anything
+				return false;
+			}
 		});
-	}
+	};
 })(jQuery);
