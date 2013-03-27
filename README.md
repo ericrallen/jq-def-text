46a5d3b9b365874e3c9a406651a654efd596dd5a
Eric Allen <decoy@internetalche.me>
2013-03-27 04:06:51 -0400

    Updated logic, added lingering default text, added destroy method, updated documentation.

--- README.md
+++ README.md
@@ -1,64 +1,125 @@
-jq-def-text (v1.3.1)
+jq-def-text (v1.4.0)
 ==================
 
-jQuery Default Text
+###jQuery Default Text
 
 This simple jQuery plug-in allows you to set default text for textboxes.
 
 It will check for HTML5 placeholder support and use that if available, and, if it is not available, it will use on focus and on blur to change the value of the textbox if nothing has been entered in the box.
 
+It checks for an already present value in the specified field and will ignore that field's default value and default class if a value is found. This will help prevent overwriting values that are inserted into the form via PHP.
+
 Check out the [Documentation](https://github.com/ericrallen/jq-def-text/wiki) for more details about configuring jq-def-text.
 
-By default, placeholder text support isn't checked via [Modernizr](http://modernizr.com/), if you would like to turn it on, use this:
-
-	$.fn.iaDefaultText.default_settings.use_modernizr = true;
-	
-Note: using Modernizr requires that the version of Modernizr you are using is configured to check inputs.
-
-You can set the placeholder text to toggle off/on like the way values are added/removed like this:
-
-	$.fn.iaDefaultText.default_settings.toggle_placeholder = true;
-
-You can also disable placeholder text and use value adding/removal like this:
-
-	$.fn.iaDefaultText.default_settings.use_placeholder = false;
-
-It can also add and remove a class when the default text is present so you can style it differently.
-
-HTML5 placeholder text does not currently support the default class functionality, you can style placeholder text using browser-prefixed styles.
-
-It checks for an already present value in the specified field and will ignore that field's default value and default class if a value is found.
-
-This will help prevent overwriting values that are inserted into the form via PHP.
+###Usage
 
 It is used like this:
 
 	$('selector').iaDefaultText({
 		def_text : 'Default Text',
-		def_class : 'classname'
+		def_class : 'classname',
+		callback : function() {
+			//on blur logic here
+		}
 	});
 
-You can also change the default options like this:
+DefaultText adds the class 'iadeftext' to the field by default.
 
-	$.fn.iaDefaultText.default_settings.def_class = 'def';
+###Some Options
 
-DefaultText adds the class 'iadeftext' to the input by default.
+####Default Text
 
-You can also get the value for an option like this:
+The most important part of the plug-in.
 
-	$('selector').iaDefaultText('def_class');
+	$.fn.iaDefaultText.default_settings.def_text = 'Default';
 
-There is also a callback function that runs after the blur function. You can specify a global callback using the default_settings, like the examples above, and you can specify a per-selector callback function when defining your options for a selector.
+####Enable/Disable Placeholder Attribute
+
+DefaultText checks for the browser's placeholder capability and uses it where applicable by default.
+
+You can disable placeholders by setting `use_placeholder` to `false`.
+
+	$.fn.iaDefaultText.default_settings.use_placeholder = false;
+
+####Placeholder Compatibility
+
+Placeholder capability can be checked by [Modernizr](http://modernizr.com/) if you already have it installed and have it configured to check inputs.
+
+You can turn Modernizr support on by setting `use_modernizr` to `true`.
+
+	$.fn.iaDefaultText.default_settings.use_modernizr = true;
+
+####Placeholder Default Class
+
+If you don't want DefaultText to add and remove the class it applies to your input when using placeholders you can set `placeholder_class` to `false`.
+
+	$.fn.iaDefaultText.default_settings.placeholder_class = false;
+
+####Toggle Placeholder Display
+
+You can set the placeholder text to show and hide like the way values are added and removed from fields in older browsers like this:
+
+	$.fn.iaDefaultText.default_settings.toggle_placeholder = true;
+
+####Lingering Placeholders and Values
+
+If you would like to have the placeholder or value stay visible for a short period after the field gains focus, you can do so like this:
+
+	$.fn.iaDefaultText.default_settings.fade_def_text = true;
+
+You can specify the time (in ms) for the default text to stay visible like this:
+
+	$.fn.iaDefaultText.default_settings.fade_timer = 500; // default text remains for 1/2 of a second
+
+####Default Class for Styling Default Text
+
+It can also add and remove a class when the default text is present so you can style it differently.
+
+	$.fn.iaDefaultText.default_settings.default_class = 'default_text'; // plug-in defaults to "iadefaultext"
+
+####Callback on Blur
+
+You can set a function to run after the field has blurred, which is useful for syncing up validation scripts or custom field logic.
+
+	$.fn.iaDefaultText.default_settings.callback = function() {
+		// logic here 
+	};
+
+###Retrieving Option Values
+
+You can get the value for an option like this:
+
+	$(selector).iaDefaultText('option name');
+
+###Resetting Option Values
+
+You can reset an option's value like this:
+
+	$(selector).iaDefaultText('option name', 'new value');
+
+###Removing Plug-in from Field
+
+You can remove DefaultText from a field like this:
+
+	$(selector).iaDefaultText('destroy');
+
+###Demo
 
 You can check out the JSFiddle for this plug-in and test it and its settings here:
 
 <http://jsfiddle.net/allenericr/bcNkw/>
 
+#Credits
+
 This plug-in was built based on the Websanova jQuery Plug In Boilerplate <http://www.websanova.com/plugins/websanova/boilerplate>.
+
+The idea for the lingering placeholders was inspired by this article <http://css-tricks.com/hang-on-placeholders/> from CSS-Tricks.
 
 change log
 ==========
  
+ - 2013-03-27 - (v1.4.0) Plug-in updated. Added ability to have values and placeholders stay visible for a period of time (defined in miliseconds). Added class addition/removal to placeholder-enabled fields. Added destroy method to allow removal of jq-def-text from an element. Updated README.md. Added comment header to minified file.
+
  - 2013-01-24 - (v1.3.1) Plug-in updated. Switched Modernizr check to defaul to false. Updated README.md. Updated file naming conventions.
 
  - 2012-08-21 - (v1.3) Plug-in updated. Switched focus and blur to be jQuery .on() events.
--- jquery.def-text/jquery.def-text.js
+++ jquery.def-text/jquery.def-text.js
@@ -5,53 +5,76 @@
  * Transmusting the Web
  *
  * @author					Eric Allen
+ * @twitter					@allenericr
  * @copyright				Copyright (c) 2012 Eric Allen
  * @license					MIT
- * @link					http://github.com/ericrallen/jq-def-text
- * @docs					http://github.com/ericrallen/jq-def-text/blob/master/README.md
- * @version					1.3.1
+ * @site					http://ericrallen.github.com/jq-def-text/
+ * @repo					http://github.com/ericrallen/jq-def-text/
+ * @docs					https://github.com/ericrallen/jq-def-text/wiki/
+ * @version					1.4.0
  *
 =====================================================*/
 
 //close out any previous JS with a semi-colon, just in case
-;(function($) {
+;(function($, window, document, undefined) {
 	$.fn.iaDefaultText = function(option, settings) {
 		//if DefaultText was called with options
 		if(typeof option === 'object') {
 			settings = option;
 		//if DefaultText was called with an option name
 		} else if(typeof option === 'string') {
-			//return the value of that option
-			var data = this.data('_iaDefaultText');
-			if(data) {
-				if($.fn.iaDefaultText.default_settings[option] !== undefined) {
-					if(settings !== undefined) {
-						data.settings[option] = settings;
-						return true;
-					} else {
-						return data.settings[option];
-					}
-				} else {
-					return false;
-				}
-			} else {
+			//create a var to store the def-text instance for this element
+			var instance = this.data('_iaDefaultText');
+
+			//if there was no def-text instance, don't do anything
+			if(!instance) {
 				return false;
+			//if there was, check the option
+			} else {
+				//first, check to see if a predefined method has been requested
+				if(option === 'destroy') {
+					//remove def-text from this element
+					instance.destroy();
+					return false;
+				//if the string wasn't a predefined method option
+				} else {
+					//make sure this option exists
+					if($.fn.iaDefaultText.default_settings[option] !== undefined) {
+						//if a value was passed with the option
+						if(settings !== undefined) {
+							//set the value for the specified option
+							instance.settings[option] = settings;
+							return true;
+						//if no value was passed
+						} else {
+							//return the value of the specified option
+							return instance.settings[option];
+						}
+					//if this option doesn't exist, don't do anything with it
+					} else {
+						return false;
+					}
+				}
 			}
 		}
 
 		settings = $.extend({}, $.fn.iaDefaultText.default_settings, settings || {});
-		
+
 		//check to see if html5 placeholder is desired
 		if(settings.use_placeholder) {
+			//default to non-placeholder support
 			$.support.placeholder = false;
-			//check to see if we should use modernizr to check for capability
+			//check for capability without modernizr
 			if(!settings.use_modernizr) {
-				test = document.createElement('input');
+				var test = document.createElement('input');
+
+				//if placeholder is available, turn on support
 				if('placeholder' in test) {
 					$.support.placeholder = true;
 				}
-			//if we should
+			//if modernizr should be used
 			} else {
+				//if modernizr reports placeholder capability, turn on support
 				if($.Modernizr.input.placeholder) {
 					$.support.placeholder = true;
 				}
@@ -59,25 +82,43 @@
 		}
 
 		return this.each(function() {
+			//cache element selector
 			var $elem = $(this);
-			//check to see if the element is a textbox or textarea
+
+			//check to see if the element is a text input or textarea
 			if($elem.is('input[type=text]') || $elem.is('textarea') || $elem.is('input[type=email]') || $elem.is('input[type=phone]') || $elem.is('input[type=url]') || $elem.is('input[type=search]') || $elem.is('input[type=password]')) {
 				var $settings = jQuery.extend(true,{},settings);
+
 				//check to see if there was a value added to the input already
 				if($elem.val() === '' || $elem.val() === $settings.def_text) {
 					var deftext = new DefaultText($settings);
+
 					//add element to object for later calls
 					deftext.deftext = $elem;
+
 					//get element id
 					var id = '#' + $elem.attr('id');
+
 					//initialize deftext
 					deftext.initialize();
-					//when the field gets focus or blur
-					$('body').on('focus', id, function() {
-						deftext.on_focus();
-					}).on('blur', id, function() {
-						deftext.on_blur();
-					});
+
+					//when the field gets focus
+					$('body').on(
+						'focusin.ia_deftext',
+						id,
+						function() {
+							deftext.on_focus();
+						}
+					//when the field gets blur
+					).on(
+						'focusout.ia_deftext',
+						id,
+						function() {
+							deftext.on_blur();
+						}
+					);
+
+					//store our def-text object in a data attribute
 					$elem.data('_iaDefaultText', deftext);
 				}
 			}
@@ -85,12 +126,15 @@
 	};
 
 	$.fn.iaDefaultText.default_settings = {
-		def_text : '',
-		def_class : 'iadeftext',
-		use_placeholder : true,
-		use_modernizr : false,
-		toggle_placeholder : false,
-		callback : ''
+		def_text : '',					//the text to display
+		def_class : 'iadeftext',		//the class to apply when default text is visible
+		use_placeholder : true,			//try to use placeholder if available
+		use_modernizr : false,			//check placeholder capability via modernizr
+		toggle_placeholder : false,		//hide/show the placeholder on blur/focus
+		callback : '',					//function to run after blur event is fired
+		fade_def_text : false,			//[v1.4.0] remove default text on focus
+		fade_time : 450,				//[v1.4.0] seconds after focus default text should hide
+		placeholder_class : true		//[v1.4.0] add/remove def_class with placeholder and value
 	};
 
 	function DefaultText(settings) {
@@ -100,57 +144,250 @@
 	}
 
 	DefaultText.prototype = {
+
+		//insert default text and add default class
 		initialize : function() {
+			//make the element and settings easy to refer to
 			var $this = this;
-			var $settings = $this.settings;
+			var settings = $this.settings;
+
+			//if there is an element
 			if($this.deftext) {
 				//if the field is empty
 				if($this.deftext.val() === '') {
-					if($.support.placeholder && $settings.use_placeholder) {
-						$this.deftext.attr('placeholder',$settings.def_text);
+					//if using placeholders
+					if($this.using_placeholders()) {
+						//insert the default value
+						$this.deftext.attr('placeholder', settings.def_text);
+
+						//if toggling class for placeholders
+						if(settings.placeholder_class) {
+							$this.toggle_class(true);
+						}
+					//if not using placeholders
 					} else {
-						//we want to insert the default value
-						$this.deftext.val($settings.def_text);
+						//insert the default value
+						$this.deftext.val(settings.def_text);
+
 						//set the default class
-						$this.deftext.addClass($settings.def_class);
+						$this.toggle_class(true);
 					}
 				}
 			}
 		},
+
+		//actions for focus event
 		on_focus : function() {
+			//make the element and settings easy to refer to
 			var $this = this;
-			var $settings = $this.settings;
-			if(!$.support.placeholder || !$settings.use_placeholder) {
-				//if the value is the default text
-				if($this.deftext.val() == $settings.def_text) {
-					//remove the text and the default class
-					$this.deftext.val("");
-					$this.deftext.removeClass($settings.def_class);
+			var settings = $this.settings;
+
+			//if using placeholders
+			if($this.using_placeholders()) {
+				//if toggling placeholders
+				if(settings.toggle_placeholder) {
+					//if fading out the default text
+					if(settings.fade_default) {
+						//set timer for toggle
+						$this.fade_timeout = setTimeout(
+							function() {
+								$this.deftext.attr('placeholder','');
+							},
+							settings.fade_time
+						);
+					} else {
+						$this.deftext.attr('placeholder','');
+					}
 				}
+
+				//if toggling class for placeholders
+					if(settings.placeholder_class) {
+						$this.toggle_class();
+					}
+			//if not using placeholders
 			} else {
-				if($settings.toggle_placeholder) {
-					$this.deftext.attr('placeholder','');
+				//if the value is the default text
+				if($this.is_default()) {
+					//if fading out the default text
+					if(settings.fade_default) {
+						//set timer for toggle
+						$this.fade_timeout = setTimeout(
+							function() {
+								$this.focus_toggle();
+							},
+							settings.fade_time
+						);
+					//if not, just toggle
+					} else {
+						$this.focus_toggle();
+					}
 				}
 			}
 		},
-		on_blur : function() {
+
+		//toggle value on focus
+		focus_toggle : function() {
+			//make the element and settings easy to refer to
 			var $this = this;
-			var $settings = $this.settings;
-			if(!$.support.placeholder || !$settings.use_placeholder) {
-				//if the value is empty
-				if($this.deftext.val() === "") {
-				//insert the default text and add the default class
-					$this.deftext.val($settings.def_text);
-					$this.deftext.addClass($settings.def_class);
+			var settings = $this.settings;
+
+			//remove the text and the default class
+			$this.deftext.val("");
+			$this.toggle_class();
+		},
+
+		//actions for blur event
+		on_blur : function() {
+			//make the element and settings easy to refer to
+			var $this = this;
+			var settings = $this.settings;
+
+			//if using placeholders
+			if($this.using_placeholders()) {
+				//if toggling placeholder, add it back in
+				if(settings.toggle_placeholder) {
+					$this.deftext.attr('placeholder', settings.def_text);
 				}
+
+				//if toggling class with placeholder
+				//check for default text and add it back in
+				if(settings.placeholder_class && $this.is_empty()) {
+					$this.toggle_class($this.deftext);
+				}
+			//if not using placeholders
 			} else {
-				if($settings.toggle_placeholder) {
-					$this.deftext.attr('placeholder',$settings.def_text);
+				//if the value is empty
+				if($this.is_empty()) {
+					//insert the default text and add the default class
+					$this.deftext.val(settings.def_text);
+					$this.toggle_class();
 				}
 			}
-			if($settings.callback && typeof $settings.callback === 'function') {
-				$settings.callback.call(this);
+
+			//if there is a callback defined
+			if(settings.callback && typeof settings.callback === 'function') {
+				settings.callback.call(this);
+			}
+		},
+
+		//[v1.4.0] switch the def_class on the element
+		//the initial paramter will stop toggling if the class is present and this is during initialization
+		toggle_class : function(initial) {
+			//make the element and settings easy to refer to
+			var $this = this;
+			var settings = $this.settings;
+
+			//default to not inital load
+			var init = false;
+
+			//if initial was set
+			if(typeof initial !== 'undefined') {
+				init = initial;
+			}
+
+			//if not initial load
+			if(!init) {
+				//if the element has the class, remove it
+				if($this.deftext.hasClass(settings.def_class)) {
+					$this.deftext.removeClass(settings.def_class);
+				//if the element doesn't have the class, add it
+				} else {
+					$this.deftext.addClass(settings.def_class);
+				}
+			//if initial load
+			} else {
+				//if the element doesn't have the class, add it
+				if(!$this.deftext.hasClass(settings.def_class)) {
+					$this.deftext.addClass(settings.def_class);
+				}
+			}
+		},
+
+		//[v1.4.0] check placeholders, returns boolean
+		using_placeholders : function() {
+			//make the element and settings easy to refer to
+			var $this = this;
+			var settings = $this.settings;
+
+			//check for placeholder support and setting
+			if($.support.placeholder && settings.use_placeholder) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+
+		//[v1.4.0] check for default text, returns boolean
+		is_default : function() {
+			//make the element and settings easy to refer to
+			var $this = this;
+			var settings = $this.settings;
+
+			//if the value is the default text
+			if($this.deftext.val() === settings.def_text) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+
+		//[v1.4.0] check for empty value, returns boolean
+		is_empty : function() {
+			//make the element and settings easy to refer to
+			var $this = this;
+			var settings = $this.settings;
+
+			//if the value is empty
+			if($this.deftext.val() === "") {
+				return true;
+			} else {
+				return false;
+			}
+		},
+
+		//[v1.4.0] remove deftext events, classes, data, etc.
+		destroy : function() {
+			var $this = this;
+			var settings = $this.settings;
+
+			//if there is an element
+			if($this.deftext) {
+				//if using placeholders, remove placeholder
+				if($this.using_placeholders()) {
+					$this.deftext.attr('placeholder', '');
+				//if not using placeholders
+				} else {
+					//if the input is displaying def_text, remove it
+					if($this.is_default()) {
+						$this.deftext.val('');
+					}
+				}
+
+				//remove def_class
+				$this.deftext.removeClass(settings.def_class);
+
+				//remove associated data attribute
+				$this.deftext.removeData('_iaDefaultText');
+
+				//get element id
+				var id = '#' + $this.deftext.attr('id');
+
+				//unbind events
+				$('body').off(
+					'focusin.ia_deftext',
+					id
+				).off(
+					'focusout.ia_deftext',
+					id
+				);
+
+				//remove element
+				$this.deftext = '';
+
+				//reset object
+				$this = {};
 			}
 		}
+
 	};
-})(jQuery);
+})(jQuery, window, document);
